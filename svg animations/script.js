039bd6f7101306689f072a8990908a88c26432f6
tanishqtiwari45 <tiwaritanishq45@gmail.com>
2025-03-04 18:52:30 +0530

    Updated Files

---
+++ svg animations/script.js
@@ -0,0 +1,9 @@
+  var initialPath = 'M 10 100 Q 250 100 490 100'
+
+  var finalPath = 'M 10 100 Q 250 100 490 100'
+
+var string = document.querySelector("string")
+
+string.addEventListener("click",function() {
+	console.log("Clicked");
+})
