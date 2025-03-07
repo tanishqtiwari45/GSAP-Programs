ba6b846cfb06f66a6ef8a32d8fe30861ca9b8448
tanishqtiwari45 <tiwaritanishq45@gmail.com>
2025-03-07 10:49:03 +0530

    Fifth Commit

---
+++ gsap-navbar-toggle/script.js
@@ -0,0 +1,28 @@
+var menu = document.querySelector("#nav i")
+var cross = document.querySelector("#full i")
+var tl = gsap.timeline({paused:true})
+
+tl.to("#full",{
+	right:0,
+	duration:0.4
+})
+tl.from("#full h4",{
+	x:150,
+	duration:0.5,
+	stagger:0.3,
+	opacity:0
+})
+tl.from("#full i",{
+	opacity:0,
+	duration:0.8
+})
+tl.pause()
+
+
+menu.addEventListener("click",function(){
+	tl.play()
+})
+
+cross.addEventListener("click",function(){
+	tl.reverse()
+})
